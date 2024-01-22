b46ac3562f8b80ed567e55b23fbcc1635e1af571
Lettie <lettiezulu764@gmail.com>
2024-01-22 15:48:32 +0200

    Lesson1-3

---
+++ NetBeansProjects/Lesson3/src/ex3_1_exercise/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex3_1_exercise;
+
+/**
+ *
+ * @author letti
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
