adfde7bbcb661760836a70cadb17953a03a45235
Lettie <lettiezulu764@gmail.com>
2024-01-25 14:26:53 +0200

    Lesson_8

---
+++ NetBeansProjects/Lesson_8/src/lesson_8/Shirt.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_8;
+
+/**
+ *
+ * @author letti
+ */
+public class Shirt {
+
+    public Shirt() {
+        System.out.println("in Shirt Constructor");
+        description = "-description required";
+        colorCode = 'U';
+        price = 0.0;
+}
+
+public String description;
+public char colorCode;
+public double price;
+
+    public void display() {
+        System.out.println("Shirt description: " + description);
+        System.out.println("Color code: " + colorCode);
+        System.out.println("Shirt price: R" + price);
+}
+
+}
---
+++ NetBeansProjects/Lesson_8/src/lesson_8/ShoppingCart.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_8;
+
+/**
+ *
+ * @author letti
+ */
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        Shirt myShirt = new Shirt();
+        myShirt.display();
+        System.out.println("the end");
+    }
+}
