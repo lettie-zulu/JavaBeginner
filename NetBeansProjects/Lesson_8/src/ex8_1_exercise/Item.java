38d88fb6ebd3fc29f7367afa7791908ed2d288fd
Lettie <lettiezulu764@gmail.com>
2024-01-28 21:09:23 +0200

    ex8_1_exercise

---
+++ NetBeansProjects/Lesson_8/src/ex8_1_exercise/Item.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex8_1_exercise;
+
+/**
+ *
+ * @author letti
+ */
+public class Item {
+    
+}
