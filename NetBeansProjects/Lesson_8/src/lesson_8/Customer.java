5bcf5c4ac1f0324aeeb4a48e071f23ce9628ca21
Lettie <lettiezulu764@gmail.com>
2024-01-28 21:24:26 +0200

    Customer.java

---
+++ NetBeansProjects/Lesson_8/src/lesson_8/Customer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_8;
+
+/**
+ *
+ * @author letti
+ */
+public class Customer {
+    public boolean isNew;
+    public boolean isNewCustomer() {
+        return isNew;
+    }
+    public void sendEmail(String message) {
+        System.out.println("send Email with message " + message);
+}
+    
+}
