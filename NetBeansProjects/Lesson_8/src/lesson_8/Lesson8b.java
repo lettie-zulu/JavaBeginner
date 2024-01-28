505d860fdaad409f1ba6bdafb30f65709d38e3c8
Lettie <lettiezulu764@gmail.com>
2024-01-28 21:31:00 +0200

    Lesson8b.java

---
+++ NetBeansProjects/Lesson_8/src/lesson_8/Lesson8b.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_8;
+
+/**
+ *
+ * @author letti
+ */
+public class Lesson8b {
+
+    public static void main(String[] args) {
+        Lesson_8 less8 = new Lesson_8();
+        double result = less8.sum(28, 99);
+        System.out.println(result);
+    }
+}
