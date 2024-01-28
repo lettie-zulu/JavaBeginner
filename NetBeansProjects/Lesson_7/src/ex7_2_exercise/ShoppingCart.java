dc723fc5c3647dac434a5f1b8333f9c4001454f3
Lettie <lettiezulu764@gmail.com>
2024-01-28 21:32:43 +0200

    ShoppingCart

--- NetBeansProjects/Lesson_7/src/ex7_2_exercise/ShoppingCart.java
+++ NetBeansProjects/Lesson_7/src/ex7_2_exercise/ShoppingCart.java
@@ -11,23 +11,23 @@ package ex7_2_exercise;
 public class ShoppingCart {
 
     public static void main(String[] args) {
-//        String custName = "Steve Smith";
-//        String firstName;
-//        int spaceIdx;
-//        StringBuilder sb;
-//
-//        spaceIdx = custName.indexOf(" ");
-//        firstName = custName.substring(0, spaceIdx);
-//        System.out.println(firstName);
-//
-//        //Instantiate and initialize sb to firstName.
-//        sb = new StringBuilder(firstName);
-//        // Put the full name back together, using StringBuilder append method.
-//        //   You can just enter the String literal for the last name.
-//        //sb.append(" Smith");
-//        sb.append(custName.substring(spaceIdx));
-//        //   Print the full name.
-//        System.out.println(sb);
+        String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        StringBuilder sb;
+
+        spaceIdx = custName.indexOf(" ");
+        firstName = custName.substring(0, spaceIdx);
+        System.out.println(firstName);
+
+        //Instantiate and initialize sb to firstName.
+        sb = new StringBuilder(firstName);
+        // Put the full name back together, using StringBuilder append method.
+        //   You can just enter the String literal for the last name.
+        //sb.append(" Smith");
+        sb.append(custName.substring(spaceIdx));
+        //   Print the full name.
+        System.out.println(sb);
 
     }
 }
