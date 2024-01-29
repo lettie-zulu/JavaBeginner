616312fb8992672daef28276e507c28085804171
Lettie <lettiezulu764@gmail.com>
2024-01-29 17:55:55 +0200

    Calculator.java

--- NetBeansProjects/Lesson_8/src/lesson_8/Calculator.java
+++ NetBeansProjects/Lesson_8/src/lesson_8/Calculator.java
@@ -9,8 +9,23 @@ package lesson_8;
  * @author letti
  */
 public class Calculator {
-    public double calculate (int x, double y) {
-        return (x/y);
-        
+//    public double calculate (int x, double y) {
+//        return (x/y);
+//        
+//    }
+     public static int sum(int num1, int num2) {
+        System.out.println("Method One");
+        return num1 + num2;
     }
+    
+      public static float sum(float num1, float num2) {
+        System.out.println("Method Two");
+        return num1 + num2;
+    }
+      
+         public static float sum(int num1, float num2) {
+        System.out.println("Method Three");
+        return num1 + num2;
+    }
+
 }
