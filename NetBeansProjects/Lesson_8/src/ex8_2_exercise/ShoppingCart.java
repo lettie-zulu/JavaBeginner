d42660f3da4f5ec81d4f68320fbd18304a970be6
Lettie <lettiezulu764@gmail.com>
2024-01-29 17:59:27 +0200

    ShoppingCart.java

--- NetBeansProjects/Lesson_8/src/ex8_2_exercise/ShoppingCart.java
+++ NetBeansProjects/Lesson_8/src/ex8_2_exercise/ShoppingCart.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ex8_2_exercise;
-
+//3. Call the 3-arg setItemFields method and then call item1.displayItem()
+//4. Call the 4-arg setItemFields method, checking the return value.  
+//     If the return value < 0, print an invalid color code message,
+//     otherwise call displayItem.
 /**
  *
  * @author letti
  */
 public class ShoppingCart {
-    
+    public static void main(String[] args) {
+                Item item1 = new Item();
+
+        // Call the 3-arg setItemFields method and then call displayItem.
+        
+        item1.setItemFields("Shirt", 20, 100.50);
+        item1.displayItem();
+        
+        // Call the 4-arg setItemFields method, checking the return value. 
+        
+        int returnVal = item1.setItemFields("Shirt", 20, 100.50, 'B');
+        if(returnVal < 0) {
+            System.out.println("Invalid color");
+        }else {
+            item1.displayItem();
+        }
+
+
+	// Test your code for both valid and invalid values
+    }
 }
