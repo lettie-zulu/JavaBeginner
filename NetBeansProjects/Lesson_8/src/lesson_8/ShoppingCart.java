042999e966737b2b940b9f17ca919f2f24a10878
Lettie <lettiezulu764@gmail.com>
2024-01-29 18:08:36 +0200

    ShoppingCart.java

--- NetBeansProjects/Lesson_8/src/lesson_8/ShoppingCart.java
+++ NetBeansProjects/Lesson_8/src/lesson_8/ShoppingCart.java
@@ -11,8 +11,20 @@ package lesson_8;
 public class ShoppingCart {
 
     public static void main(String[] args) {
-        Shirt myShirt = new Shirt();
-        //myShirt.display();
-        System.out.println("the end");
+//        Shirt shirt01 = new Shirt("Sailor", "Blue", 30);
+//        Shirt shirt02 = new Shirt("Sweatshirt", "Green", 25);
+//        Shirt shirt03 = new Shirt("Skull Tee", "Blue", 15);
+//        Shirt shirt04 = new Shirt("Tropical", "Red", 20);
+
+        Shirt myShirt = new Shirt("Sailor", "Green", 85);
+        System.out.println("Shirt color: " + myShirt.colorCode);
+        changeShirtColor(myShirt, 'B');
+        System.out.println("Shirt color changed to: " + myShirt.colorCode);
+        
+    }
+    
+    public static void changeShirtColor(Shirt theShirt, char color) {
+        theShirt = new Shirt("Skull Tee", "Yellow", 150);
+        theShirt.colorCode = color;
     }
 }
