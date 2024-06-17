931dc3a0f0ba4176fa93452f139bdfe1b2a684c7
Lettie <lettiezulu764@gmail.com>
2024-06-17 10:12:10 +0200

    Dummies

--- NetBeansProjects/Dummies/src/book2_chap01/ClassApp.java
+++ NetBeansProjects/Dummies/src/book2_chap01/ClassApp.java
@@ -2,16 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package helloapp;
-import javax.swing.JOptionPane;
+package book2_chap01;
+
 /**
  *
  * @author letti
  */
-public class Greeter {
-    public void sayHello()
-    {
-        JOptionPane.showMessageDialog(null, "hello,world","Greeter",JOptionPane.INFORMATION_MESSAGE);
+public class ClassApp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi");
+        }
     }
-    
 }
--- NetBeansProjects/Dummies/src/book2_chap02/EnumTest.java
+++ NetBeansProjects/Dummies/src/book2_chap02/EnumTest.java
@@ -2,15 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package helloapp;
+package book2_chap02;
 
 /**
  *
  * @author letti
  */
-public class HelloApp2 {
+public class EnumTest {
+    public enum CardSuit {HEARTS, SPADES, CLUBS, DIAMONDS};
     public static void main(String[] args) {
-        Greeter myGreeterObject = new Greeter();
-        myGreeterObject.sayHello();
+        CardSuit suit;
+        suit = CardSuit.HEARTS;
+        System.out.println("The suit is " + suit);
     }
 }
