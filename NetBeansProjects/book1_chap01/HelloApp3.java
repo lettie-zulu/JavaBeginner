1b8b2de10a08a5c4dc69ba129680c2eeab95dd92
Lettie <lettiezulu764@gmail.com>
2024-06-19 14:56:09 +0200

    NetBeansProjects/book1_chap01/

---
+++ NetBeansProjects/book1_chap01/HelloApp3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book1_chap01;
+
+/**
+ *
+ * @author letti
+ */
+public class HelloApp3 {
+    public static void main(String[] args) {
+        System.out.println(args[0] + " " + args[2]);
+    }
+}
